665707b8dd29038c57a762c4320ad4a152150b08
matamong <matamatamong@gmail.com>
2022-12-27 23:28:46 +0900

    :white_check_mark: set pytest conf & test the pytest

--- app/tests/api/test_users.py
+++ app/tests/api/test_users.py
@@ -1,29 +1,27 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from app.core.config import get_app_settings
-
-APP_SETTINGS = get_app_settings()
+from app.core.config import settings
 
 
 def test(client: TestClient, db: Session) -> None:
-    print(f"{APP_SETTINGS.api_prefix}/hello")
-    response = client.get(f"{APP_SETTINGS.api_prefix}/hello")
-    assert response.status_code == 200
+    response = client.get(f"{settings.API_V1_STR}/hello")
     content = response.json()
-    print(content)
 
 
-def test_create_user(
-        client: TestClient, db: Session
-) -> None:
-    data = {"email": "matamong@matamong.com", "nickname": "matamong_is_awsome"}
-    response = client.post(
-        f"{APP_SETTINGS.api_prefix}/users/",
-        json=data,
-    )
-    assert response.status_code == 200
-    content = response.json()
-    assert content["email"] == data["email"]
-    assert content["nickname"] == data["email"]
-    assert "id" in content
+###
+# TDD ing...
+###
+# def test_create_user(
+#         client: TestClient, db: Session
+# ) -> None:
+#     data = {"email": "matamong@matamong.com", "nickname": "matamong_is_awsome"}
+#     response = client.post(
+#         f"{settings.API_V1_STR}/users/",
+#         json=data,
+#     )
+#     assert response.status_code == 200
+#     content = response.json()
+#     assert content["email"] == data["email"]
+#     assert content["nickname"] == data["email"]
+#     assert "id" in content
