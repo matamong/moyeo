015cbf7402017965cef2c7bddb4d4012ee1a3b26
matamong <matamatamong@gmail.com>
2023-04-24 04:08:25 +0900

    ::white_check_mark: add get party test

--- app/tests/crud/test_party.py
+++ app/tests/crud/test_party.py
@@ -21,5 +21,19 @@ def test_create_party(db: Session) -> None:
     assert party.leader_id == user.id
 
 
-# def test_get_party(db: Session) -> None:
-#     pass
+def test_get_party(db: Session) -> None:
+    name = random_lower_string()
+    nickname = random_lower_string()
+    desc = random_lower_string()
+    img_path = random_lower_string()
+    party_in = PartyCreate(name=name, nickname=nickname, desc=desc, img_path=img_path)
+    user = create_random_user(db=db)
+    party = crud.party.create_with_leader(db=db, obj_in=party_in, leader_id=user.id)
+    stored_party = crud.party.get(db=db, id=party.id)
+
+    assert stored_party
+    assert party.id == stored_party.id
+    assert party.name == stored_party.name
+    assert party.desc == stored_party.desc
+    assert party.img_path == stored_party.img_path
+    assert party.leader_id == stored_party.leader_id
