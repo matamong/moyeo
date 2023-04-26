ae975ba59fb15c729fb98053042195c6b3c6c54e
matamong <matamatamong@gmail.com>
2023-04-27 00:55:37 +0900

    :white_check_mark: add party api test & party test utils

--- app/tests/api/test_party.py
+++ app/tests/api/test_party.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
 
 from app.core.config import settings
+from app.schemas import PartyCreate
+from app.tests.utils.party import create_random_party_with_user, create_random_party_with_random_user, \
+    create_random_party_user
+from app.tests.utils.user import create_random_user, authentication_token_from_email
+from app.tests.utils.utils import random_lower_string
 
 
 # TODO Super User only
@@ -11,21 +16,108 @@ def read_parties():
     pass
 
 
-def test_get_my_parties(client: TestClient, normal_user_token_headers: Dict[str, str]) -> None:
-    r = client.get(f"{settings.API_V1_STR}/party/me", headers=normal_user_token_headers)
-    pass
+def test_get_my_parties(db: Session, client: TestClient) -> None:
+    user = create_random_user(db=db)
+    headers = authentication_token_from_email(db=db, client=client, email=user.email)
+
+    response = client.get(f"{settings.API_V1_STR}/party/me", headers=headers)
+    content = response.json()
+
+    assert len(content) == 0
+    assert response.status_code == 200
+
+    party_nickname1 = random_lower_string()
+    party = create_random_party_with_user(db=db, user=user, party_nickname=party_nickname1)
+    response2 = client.get(f"{settings.API_V1_STR}/party/me", headers=headers)
+    content2 = response2.json()
+
+    assert response2.status_code == 200
+    assert len(content2) == 1
+    assert content2[0]["id"] == party.id
+    assert content2[0]["name"] == party.name
+    assert content2[0]["desc"] == party.desc
+    assert content2[0]["img_path"] == party.img_path
+    assert content2[0]["leader_id"] == party.leader_id
+    assert content2[0]["code"] is not None
+    assert content2[0]["access_code"] is not None
+    assert content2[0]["is_private"] is not None
+    assert content2[0]["created_at"] is not None
+
+    party_nickname2 = random_lower_string()
+    party2 = create_random_party_with_user(db=db, user=user, party_nickname=party_nickname2)
+    response3 = client.get(f"{settings.API_V1_STR}/party/me", headers=headers)
+    content3 = response3.json()
+
+    assert response3.status_code == 200
+    assert len(content3) == 2
+    assert content3[1]["id"] == party2.id
+    assert content3[1]["name"] == party2.name
+    assert content3[1]["desc"] == party2.desc
+    assert content3[1]["img_path"] == party2.img_path
+    assert content3[1]["leader_id"] == party2.leader_id
+    assert content3[1]["code"] is not None
+    assert content3[1]["access_code"] is not None
+    assert content3[1]["is_private"] is not None
+    assert content3[1]["created_at"] is not None
 
 
-def test_get_party_by_id():
-    pass
+def test_get_party_by_id(db: Session, client: TestClient) -> None:
+    party = create_random_party_with_random_user(db=db)
+    response = client.get(f"{settings.API_V1_STR}/party/{party.id}")
+    content = response.json()
+
+    assert response.status_code == 200
+    assert content["id"] == party.id
+    assert content["name"] == party.name
+    assert content["desc"] == party.desc
+    assert content["img_path"] == party.img_path
+    assert content["code"] is not None
+    assert content["access_code"] is not None
+    assert content["is_private"] is not None
+    assert content["created_at"] is not None
 
 
-def test_get_party_with_users():
-    pass
+def test_get_party_with_users(db: Session, client: TestClient) -> None:
+    leader = create_random_user(db=db)
+    leader_party_nickname = random_lower_string()
+    party = create_random_party_with_user(db=db, user=leader, party_nickname=leader_party_nickname)
+    party_user = create_random_party_user(db=db, party_id=party.id)
+    response = client.get(f"{settings.API_V1_STR}/party/party-with-users/{party.id}")
+    content = response.json()
+
+    assert response.status_code == 200
+    assert content["id"] == party.id
+    assert content["name"] == party.name
+    assert content["desc"] == party.desc
+    assert content["leader_id"] == leader.id
+    assert content["img_path"] == party.img_path
+    assert content["code"] is None
+    assert content["access_code"] is None
+    assert content["is_private"] is not None
+    assert content["created_at"] is not None
+
+    assert len(content["party_user_set"]) == 2
+    assert content["party_user_set"][0]["nickname"] == leader_party_nickname
+    assert content["party_user_set"][0]["is_manager"] is True
+    assert content["party_user_set"][1]["id"] == party_user.id
+    assert content["party_user_set"][1]["nickname"] == party_user.nickname
+    assert content["party_user_set"][1]["is_manager"] is False
 
 
-def test_get_party_by_code():
-    pass
+def test_get_party_by_code(db: Session, client: TestClient) -> None:
+    party = create_random_party_with_random_user(db=db)
+    response = client.get(f"{settings.API_V1_STR}/party/code/{party.code}")
+    content = response.json()
+
+    assert response.status_code == 200
+    assert content["id"] == party.id
+    assert content["name"] == party.name
+    assert content["desc"] == party.desc
+    assert content["img_path"] == party.img_path
+    assert content["code"] is not None
+    assert content["access_code"] is not None
+    assert content["is_private"] is not None
+    assert content["created_at"] is not None
 
 
 def test_create_party(db: Session, client: TestClient, normal_user_token_headers: Dict[str, str]) -> None:
---
+++ app/tests/utils/party.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.models import Party, User
+from app.models.party import PartyUser
+from app.schemas import PartyCreate, UserCreate, PartyUserCreate
+from app.tests.utils.user import create_random_user
+from app.tests.utils.utils import random_lower_string
+
+
+def create_random_party_with_random_user(db: Session) -> Party:
+    user = create_random_user(db=db)
+    name = random_lower_string()
+    desc = random_lower_string()
+    img_path = random_lower_string()
+    nickname = random_lower_string()
+    party_in = PartyCreate(name=name, nickname=nickname, desc=desc, img_path=img_path)
+    party = crud.party.create_with_leader(db=db, obj_in=party_in, leader_id=user.id)
+    return party
+
+
+def create_random_party_with_user(db: Session, user: User, party_nickname: str) -> Party:
+    user = crud.user.get(db=db, id=user.id)
+    if user is None:
+        user_in = UserCreate(email=user.email, nickname=user.nickname)
+        user = crud.user.create(db=db, obj_in=user_in)
+    name = random_lower_string()
+    desc = random_lower_string()
+    img_path = random_lower_string()
+    party_in = PartyCreate(name=name, desc=desc, img_path=img_path, nickname=party_nickname)
+    return crud.party.create_with_leader(db=db, obj_in=party_in, leader_id=user.id)
+
+
+def create_random_party_user(db: Session, party_id: int) -> PartyUser:
+    user = create_random_user(db=db)
+    nickname = random_lower_string()
+    party_user_in = PartyUserCreate(party_id=party_id, nickname=nickname)
+    return crud.party_user.create_party_user(db=db, obj_in=party_user_in, user_id=user.id)
+
